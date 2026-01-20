f60af6d50590bb14f09d9418e5f2db7226f58557
Davis Sajimon <116802469+davissajimon@users.noreply.github.com>
2026-01-20 11:16:49 +0530

    backend error fixing p1

--- src/Home.jsx
+++ src/Home.jsx
@@ -256,7 +256,7 @@ export default function Home() {
         ]);
       }
     };
-
+  
     fetchSensors();
     const interval = setInterval(fetchSensors, 10000);
     return () => clearInterval(interval);
